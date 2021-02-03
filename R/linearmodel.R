1f6e99f0ec4120d2a42b9170f6bcef890efa642b
Gray Stanton <gstanton@sas.upenn.edu>
2021-02-02 18:13:21 -0700

    Updated simulation code

--- R/linearmodel.R
+++ R/linearmodel.R
@@ -23,3 +23,49 @@ simp_lin_R  <- function(x, y){
   output <- simp_lin_cpp(x, y)
   return(output)
 }
+
+#' Simulate a dataset and run simple linear regression
+#'
+#' @param n "Number of observations to simulate"
+#' @param method "Which regression function to use, either 'lm' or 'simplin'
+#' @param id "run id number
+#' @param outputprefix "prefix of output file name"
+#' @param writeout "Whether or not to write a summary of the results to a csv file"
+#' @export
+simulate <- function(n, method="lm", id=0, outputprefix="run_", writeout=TRUE){
+  inittime <- proc.time()[3]
+  x <- rnorm(n, 0, 1)
+  err <- rnorm(n, 0, 1)
+  y <- 1 + -1*x + err
+  if(method=="lm"){
+    fit <- lm(y ~ x)
+    sumfit <- summary(fit)
+    cis <- confint(fit)
+    output <- list(b0=sumfit$coefficients[1,1],
+                   b1=sumfit$coefficients[2,1],
+                   b0_se=sumfit$coefficients[1,2],
+                   b1_se=sumfit$coefficients[2,2],
+                   b0_ci=c(cis[1,1], cis[1,2]),
+                   b1_ci=c(cis[2,1], cis[2,2]),
+                   resids=resid(fit),
+                   yhat=fitted(fit))
+  }
+  else{
+    output <- simp_lin_R(x, y)
+  }
+  elapsed_time <- proc.time()[3] - inittime
+  outdf <- data.frame(method=method, id=id, runtime=elapsed_time,
+                      b0=output[["b0"]], b1=output[["b1"]],
+                      b0_lower=output[["b0_ci"]][1], b0_upper= output[["b0_ci"]][1],
+                      b1_lower=output[["b1_ci"]][1], b1_upper= output[["b1_ci"]][1],
+                      pred_mse = mean((output[["yhat"]] - output[["resids"]])^2)
+  )
+  if(writeout){
+    outfile <- paste0("./", outputprefix, id, ".csv")
+    write.csv(outdf, outfile)
+    return(outdf)
+  } else{
+    return(outdf)
+  }
+
+}
